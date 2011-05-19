e43de08b7dc47c009f0cc3c8d61df9915d77e08c
tobiasv@gmx.de <wetter-rosstal@roschtl.de>
2011-05-19 20:54:06 +0200

    update script

---
+++ includes/update.inc.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+    MyBlitzortung - a tool for participants of blitzortung.org
+	to display lightning data on their web sites.
+
+    Copyright (C) 2011  Tobias Volgnandt
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+function bo_check_for_update()
+{
+	
+	$updates = array('0.2.2' => 202);
+	$cur_version = bo_get_conf('version');
+
+	preg_match('/([0-9]+)(\.([0-9]+)(\.([0-9]+))?)?/', $cur_version, $r);
+	$cur_version_num = $r[1] * 10000 + $r[3] * 100 + $r[5];
+
+	if ($cur_version_num < max($updates) && $_GET['bo_action'] != 'do_update')
+	{
+		echo 'Database version changed! ';
+		echo '<a href="'.bo_insert_url('bo_action', 'do_update').'">Click to update</a>';
+		bo_copyright_footer();
+		return true;
+	}
+	
+	$updated = false;
+	
+	foreach($updates as $new_version => $number)
+	{
+		if ($cur_version_num >= $number)
+			continue;
+		
+		echo '<h4>Updating from '.$cur_version.' to '.$new_version.'</h4>';
+		
+		echo '<ul>';
+		
+		$ok = false;
+		switch ($new_version)
+		{
+			case '0.2.2':
+			
+				bo_db('ALTER TABLE '.BO_DB_PREF.'raw DROP INDEX `time`', false); // to be sure the key is not added twice
+			
+				$sql = 'ALTER TABLE '.BO_DB_PREF.'raw ADD INDEX (`time`)';
+				$ok = bo_db($sql);
+				
+				echo '<li><em>'.$sql.'</em>: <b>'.($ok ? 'OK' : 'FAIL').'</b></li>';
+				
+				$ok = true; //doesn't matter too much if this fails ;-)
+				break;
+		}
+		
+		echo '</ul>';
+		
+		if ($ok)
+		{
+			bo_set_conf('version', $new_version);
+			$cur_version = $new_version;
+			$cur_version_num = $number;
+			$updated = true;
+		}
+		
+		
+	}
+	
+	if ($updated)
+	{
+		echo '<h4>Update done!</h4>';
+	}
+	
+	
+}
+
+
+?>
